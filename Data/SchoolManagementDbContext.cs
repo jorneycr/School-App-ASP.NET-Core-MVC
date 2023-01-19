7490a5f3f9e34983085ef7e163cb35245e0ee90c
jorneylopez <jorney95@live.com>
2023-01-18 21:19:24 -0600

    Added some controllers

--- Data/SchoolManagementDbContext.cs
+++ Data/SchoolManagementDbContext.cs
@@ -11,7 +11,11 @@ public partial class SchoolManagementDbContext : DbContext
     {
     }
 
+    public virtual DbSet<Class> Classes { get; set; }
+
     public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
 
     public virtual DbSet<Lecturer> Lecturers { get; set; }
 
@@ -19,6 +23,19 @@ public partial class SchoolManagementDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Class>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Classes__3214EC073565AD88");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK__Classes__CourseI__3F466844");
+
+            entity.HasOne(d => d.Lecturer).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.LecturerId)
+                .HasConstraintName("FK__Classes__Lecture__3E52440B");
+        });
+
         modelBuilder.Entity<Course>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC07F5D9C674");
@@ -27,6 +44,21 @@ public partial class SchoolManagementDbContext : DbContext
 
             entity.Property(e => e.Code).HasMaxLength(5);
             entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Enrollme__3214EC0777B50869");
+
+            entity.Property(e => e.Grade).HasMaxLength(2);
+
+            entity.HasOne(d => d.Class).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.ClassId)
+                .HasConstraintName("FK__Enrollmen__Class__4316F928");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .HasConstraintName("FK__Enrollmen__Stude__4222D4EF");
         });
 
         modelBuilder.Entity<Lecturer>(entity =>
--- Data/Student.cs
+++ Data/Student.cs
@@ -6,10 +6,12 @@ namespace SchoolManagementApp.MVC.Data;
 public partial class Student
 {
     public int Id { get; set; }
-    
+
     public string FirstName { get; set; } = null!;
 
     public string LastName { get; set; } = null!;
 
     public DateTime? DateOfBirth { get; set; }
+
+    public virtual ICollection<Enrollment> Enrollments { get; } = new List<Enrollment>();
 }
