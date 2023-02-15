25d93b0f1fb5920783e463015a6e2d928fd7611f
jorneylopez <jorney95@live.com>
2023-02-15 00:36:12 -0600

    Added some models, and removed some code

---
+++ Models/StudentEnrollmentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolManagementApp.MVC.Models;
+
+public class StudentEnrollmentViewModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsEnrolled { get; set; }
+}
